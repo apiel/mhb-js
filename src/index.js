6d6580a071a87eb123f3113d0cf9e62f9a20f0df
Alexandre Piel <alexandre.piel@gmail.com>
2018-11-12 22:50:24 +0100

    sssssssssssssssssssssssssssdp

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
-const zigbee = require('./zigbee/zigbee.js');
-const rf = require('./rf/rf.js');
+// const zigbee = require('./zigbee/zigbee.js');
+// const rf = require('./rf/rf.js');
+require('./huebridge/huebridge.js');
 
-rf(zigbee);
+// rf(zigbee);
 
 // 0x000b57fffe277918 dimmer
 // 0x00158d00020a3941 e14 bulb
