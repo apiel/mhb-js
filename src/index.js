649e85e04a00b44e483b179322ba8a1188fed051
Alexandre Piel <alexandre.piel@gmail.com>
2018-12-06 20:54:53 +0100

    milight

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 require('./milight/milight');
-// require('./zigbee/zigbee');
-// require('./rf/rf');
-// require('./bridge/bridge');
+require('./zigbee/zigbee');
+require('./rf/rf');
+require('./bridge/bridge');
 
 // 0x000b57fffe277918 dimmer
 // 0x00158d00020a3941 e14 bulb
