ae465defb6dfcd1a8424dd9e1a6233b43b81d0c2
alex rpi <alexandre.piel@gmail.com>
2019-01-16 19:57:12 +0000

    typo

--- src/zigbee/settings.js
+++ src/zigbee/settings.js
@@ -13,7 +13,7 @@ module.exports = {
         IKEA_E27_BULB_TRIANGLE: { addr: '0x000b57fffe3046b8', name: 'triangle light', type: types.light.name },
         IKEA_DIMMER_SOFA: { addr: '0x000b57fffe150865', name: 'dimmer sofa', type: types.remote.name },
         XIAOMI_BTN_KITCHEN: { addr: '0x00158d0002016173', name: 'kitchen button', type: types.remote.name },
-        XIAOMI_BTN_BATHROON: { addr: '0x00158d0002131199', name: 'bathroom button', type: types.remote.name
+        XIAOMI_BTN_BATHROON: { addr: '0x00158d0002131199', name: 'bathroom button', type: types.remote.name },
     },
     actions: {
         onOff: (state = 'on') => ({ state }),
