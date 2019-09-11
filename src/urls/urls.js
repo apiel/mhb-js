299f7f148e29c2dd74a161cb77f6afe802b7153c
Alexandre Piel <alexandre.piel@gmail.com>
2019-09-11 21:54:22 +0200

    up entrance light ip

--- src/urls/urls.js
+++ src/urls/urls.js
@@ -3,9 +3,9 @@ const { URL } = require('url');
 const { timeLimitIsOver } = require('../utils');
 
 const urls = {
-    LIGHT_WALL_ENTRANCE_TOGGLE: { url: 'http://192.168.0.102/toggle', limitCall: 2000 },
-    LIGHT_WALL_ENTRANCE_ON: { url: 'http://192.168.0.102/on', limitCall: 2000 },
-    LIGHT_WALL_ENTRANCE_OFF: { url: 'http://192.168.0.102/off', limitCall: 2000 },
+    LIGHT_WALL_ENTRANCE_TOGGLE: { url: 'http://192.168.0.220/toggle', limitCall: 2000 },
+    LIGHT_WALL_ENTRANCE_ON: { url: 'http://192.168.0.220/on', limitCall: 2000 },
+    LIGHT_WALL_ENTRANCE_OFF: { url: 'http://192.168.0.220/off', limitCall: 2000 },
     LIGHT_KITCHEN_TOGGLE: { url: 'http://192.168.0.94/toggle', limitCall: 2000 },
     LIGHT_KITCHEN_OFF: { url: 'http://192.168.0.94/off', limitCall: 2000 },
     LIGHT_KITCHEN_ON: { url: 'http://192.168.0.94/on', limitCall: 2000 },
@@ -18,12 +18,10 @@ const urls = {
 };
 
 // call every 5 seconds url to keep awake connection
-/*
 const uniqOriginUrls = [...new Set(Object.values(urls).map(({ url }) => (new URL(url)).origin))];
 setInterval(() => {
     uniqOriginUrls.forEach(url => axios({ url }).catch(() => {}));
 }, 60000);
-*/
 
 function call(options) {
     if (options.limitCall && !timeLimitIsOver(options.url, options.limitCall)) {
