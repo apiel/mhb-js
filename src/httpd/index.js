2cf6e2455cc644eb27fe83238de30fa18972e63f
apiel rpi <alexandre.piel@gmail.com>
2019-09-14 22:08:16 +0100

    httpd

---
+++ src/httpd/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+// const bodyParser = require('body-parser');
+
+const app = express();
+
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json());
+// app.use(bodyParser.raw({ type: '*' }));
+// app.use(bodyParser.text());
+
+app.all('/esp/button', handleEspButton);
+
+app.use((req, res, next) => {
+    console.log('No route for ', req.originalUrl, req.method);
+    res.status(404).send('Sorry cant find that!');
+});
+
+function handleEspButton(req, res) {
+    const { params, body, query }  = req;
+    console.log('>> handleEspButton', { params, body, query });
+
+    res.json([{ success: true }]);
+}
+
+app.listen(3000, () => console.log('Httpd listen on port 3000'));
--- src/index.js
+++ src/index.js
@@ -3,3 +3,4 @@ require('./zigbee/zigbee');
 //require('./rf/rf');
 require('./bridge/bridge');
 require('./schedule');
+require('./httpd');
