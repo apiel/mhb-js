8856c83af07e5451698c32d9869119d1cd57b454
apiel rpi <alexandre.piel@gmail.com>
2019-11-24 11:49:32 +0000

    add action for room

--- src/httpd/action.js
+++ src/httpd/action.js
@@ -6,6 +6,7 @@ const { allLivingRoomOff } = require('../scene/all');
 
 const devices = {
     LIVING_ROOM_LIGHT: { mac: '5C:CF:7F:98:56:CD' },
+    ROOM_LIGHT: { mac: '5C:CF:7F:1C:2A:C9' },
 };
 
 function handleEspButton(req, res) {
@@ -28,6 +29,19 @@ function handleEspButton(req, res) {
             );
         } else if (query.btn1 === '-1' || query.btn2 === '-1') {
             allLivingRoomOff();
+        }
+    }
+
+    if (query.mac === devices.ROOM_LIGHT.mac) {
+        console.log('>> btn ROOM_LIGHT');
+        if (query.btn2 === '-1') {
+            advanceActions.brightness(zigbee.devices.INNR_E14_BULB, 255);
+        } else if (query.btn2 === '2') {
+            advanceActions.brightness(zigbee.devices.INNR_E14_BULB, 10);
+        } else if (query.btn2 === '3') {
+            advanceActions.brightness(zigbee.devices.INNR_E14_BULB, 80);
+        } else if (query.btn2 === '4') {
+            call(urls.LIGHT_WALL_ENTRANCE_TOGGLE);
         }
     }
 
