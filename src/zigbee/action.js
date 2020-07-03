330f45a0d4720b4d15b61518f81d4f6777ec1da4
Alexandre Piel <alexandre.piel@gmail.com>
2020-07-03 23:59:09 +0200

    new devices

--- src/zigbee/action.js
+++ src/zigbee/action.js
@@ -11,6 +11,13 @@ async function hold(addr) {
     setBrightness(addr, bri < 200 ? 255 : 10);
 }
 
+async function rotate(addr, angle) {
+    const bri = await getBrightness(addr);
+    const newBri = bri + angle;
+    console.log('Rotate', { bri, newBri, angle });
+    setBrightness(addr, newBri);
+}
+
 function action(addr, payload) {
     console.log('action device', addr, payload);
     if (addr === devices.IKEA_ONOFF.addr || addr === devices.IKEA_ONOFF2.addr) {
@@ -33,6 +40,26 @@ function action(addr, payload) {
             console.log('opple btn1 hold');
         }
         // till 6
+    } else if (addr === devices.XIAOMI_CUBE.addr) {
+        if (payload.action === 'shake') {
+            setOnOff(devices.IKEA_OUTLET_HALLWAY.addr, 'off');
+            setOnOff(devices.INNR_E14_BULB.addr, 'off');
+        } else if (payload.action === 'flip90') {
+            setOnOff(devices.INNR_E14_BULB.addr, 'toggle');
+        } else if (
+            payload.action === 'rotate_right' ||
+            payload.action === 'rotate_left'
+        ) {
+            rotate(devices.INNR_E14_BULB.addr, payload.angle);
+        }
+    } else if (addr === devices.XIAOMI_BTN_ENTRANCE.addr) {
+        if (payload.click === 'single') {
+            setOnOff(devices.INNR_E14_BULB.addr, 'toggle');
+        } else if (payload.click === 'double') {
+            setOnOff(devices.IKEA_OUTLET_HALLWAY.addr, 'toggle');
+        } else if (payload.action === 'hold') {
+            hold(devices.INNR_E14_BULB.addr);
+        }
     }
 }
 
