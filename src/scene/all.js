7d619587c0850fd0e7f66ec7a36af99180c37640
Alex from rpi <alexandre.piel@gmail.com>
2020-12-21 20:39:37 +0000

    trylog all

--- src/scene/all.js
+++ src/scene/all.js
@@ -3,18 +3,26 @@ const devices = require('../zigbee/devices');
 const urls = require('../urls/urls');
 const { call } = urls;
 
+function trylog(fn) {
+    try {
+       return fn();
+    } catch (e) {
+       console.error(e);
+    }
+}
+
 function allLivingRoomOff() {
     console.log('devices', devices);
-    setOnOff(devices.IKEA_E27_BULB_SOFA.addr, 'off');
-    setOnOff(devices.IKEA_E27_BULB_TRIANGLE.addr, 'off');
-    call(urls.LIGHT_KITCHEN_OFF);
-    call(urls.LIGHT_LIVING_ROOM_OFF);
+    trylog(() => setOnOff(devices.IKEA_E27_BULB_SOFA.addr, 'off'));
+    trylog(() => setOnOff(devices.IKEA_E27_BULB_TRIANGLE.addr, 'off'));
+    trylog(() => call(urls.LIGHT_KITCHEN_OFF));
+    trylog(() => call(urls.LIGHT_LIVING_ROOM_OFF));
 }
 
 function allFlatOff() {
-    allLivingRoomOff();
-    call(urls.LIGHT_WALL_ENTRANCE_OFF);
-    call(urls.LIGHT_UNDER_OFF);
+    trylog(() => allLivingRoomOff());
+    trylog(() => call(urls.LIGHT_WALL_ENTRANCE_OFF));
+    trylog(() => call(urls.LIGHT_UNDER_OFF));
 }
 
 module.exports = {
